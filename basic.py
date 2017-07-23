931a3bc50f3f07857ea243f025b17815b94a04ee
will-lzx <will-liu@live.com>
2017-07-23 14:37:35 +0800

    add ticket

--- basic.py
+++ basic.py
@@ -12,15 +12,33 @@ def real_get_access_token():
     appSecret = "4fa59c6b06441dec0238fbf0df841c63"
 
     postUrl = ("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid={0}&secret={1}".format(appId, appSecret))
+    try:
+        req = urllib.request.Request(postUrl)
+        res = urllib.request.urlopen(req)
+        urlResp = json.loads(res.read())
 
-    req = urllib.request.Request(postUrl)
-    res = urllib.request.urlopen(req)
-    urlResp = json.loads(res.read())
+        accessToken = urlResp['access_token']
+        leftTime = urlResp['expires_in']
 
-    accessToken = urlResp['access_token']
-    leftTime = urlResp['expires_in']
+        update_access_token(accessToken, leftTime)
+    except Exception as ex:
+        print(ex)
 
-    update_access_token(accessToken, leftTime)
+
+def real_get_jsapi_ticket(access_token):
+    postUrl = ("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token={0}&type=jsapi".format(access_token))
+
+    try:
+        req = urllib.request.Request(postUrl)
+        res = urllib.request.urlopen(req)
+        urlResp = json.loads(res.read())
+
+        ticket = urlResp['ticket']
+        leftTime = urlResp['expires_in']
+
+        update_jsapi_ticket(ticket, leftTime)
+    except Exception as ex:
+        print(ex)
 
 
 def update_access_token(token, leftTime):
@@ -28,9 +46,19 @@ def update_access_token(token, leftTime):
     mysql.exec_none_query('update access_token set token="{0}", expire_time={1} where id=1'.format(token, leftTime))
 
 
+def update_jsapi_ticket(ticket, leftTime):
+    mysql = MySQL()
+    mysql.exec_none_query('update access_token set token="{0}", expire_time={1} where id=2'.format(ticket, leftTime))
+
+
 def update_leftTime(leftTime):
     mysql = MySQL()
     mysql.exec_none_query('update access_token set expire_time={0} where id=1'.format(leftTime))
+
+
+def update_ticket_leftTime(leftTime):
+    mysql = MySQL()
+    mysql.exec_none_query('update access_token set expire_time={0} where id=2'.format(leftTime))
 
 
 def get_leftTime():
@@ -39,13 +67,36 @@ def get_leftTime():
     return leftTime
 
 
+def get_ticket_leftTime():
+    mysql = MySQL()
+    leftTime = int(mysql.exec_query('select expire_time from access_token where id=2')[0][0])
+    return leftTime
+
+
+def get_accecc_token():
+    sql = 'select token from access_token where id=1'
+    mysql = MySQL()
+    access_token = mysql.exec_query(sql)[0][0]
+    return access_token
+
+
 def run():
+    left = 1800
+    internal_time = 30
     leftTime = get_leftTime()
-    if leftTime > 1800:
-        leftTime -= 30
+    if leftTime > left:
+        leftTime -= internal_time
         update_leftTime(leftTime)
     else:
         real_get_access_token()
+
+    access_token = get_accecc_token()
+    ticket_lefttime = get_ticket_leftTime()
+    if ticket_lefttime > left:
+        ticket_lefttime -= internal_time
+        update_ticket_leftTime(ticket_lefttime)
+    else:
+        real_get_jsapi_ticket(access_token)
 
 
 if __name__ == '__main__':
